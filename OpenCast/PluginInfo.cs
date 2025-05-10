40f17bbb3e97997474e6de5efe5e4b8cdd0d6842
Graze <82724623+The-Graze@users.noreply.github.com>
2025-05-10 21:09:47 +0100

    Add project files.

---
+++ OpenCast/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenCast
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.various.gorillatag.opencast";
+        public const string Name = "OpenCast";
+        public const string Version = "1.0.0";
+    }
+}
